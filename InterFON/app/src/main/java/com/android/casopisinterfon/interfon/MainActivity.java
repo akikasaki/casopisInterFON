224da0b13ec57e7b7b354737db2cd550e7698727
Radovan Ristovic <radovanr995@gmail.com>
2017-03-04 14:50:01 +0100

    Initial android studio commit.

---
+++ InterFON/app/src/main/java/com/android/casopisinterfon/interfon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.casopisinterfon.interfon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
